e10ca1b190f8f6b28f69af96e348351eabb955ab
0vev0 <43699772+0vev0@users.noreply.github.com>
2020-04-15 18:36:11 -0700

    Update Ising_instruction.py

--- Ising_instruction.py
+++ Ising_instruction.py
@@ -8,21 +8,6 @@ import matplotlib.pyplot as plt
 import scipy.special as sc
 
 #Simulation part
-def initialize_lattice(N):
-    ''' 
-    The initialization of lattice should be the first step of simulation. In this function, 
-    we give the lattice a random initial state by built-in function, the 
-    
-    arguments:
-    N: the number of lattice points
-    
-    return values:
-    initial_lattice_state: the initial state generalized by this function
-                           it a numpy array whose elements equals to +1 or -1
-    '''
-    return initial_lattice_state
-
-
 def spin_flip(lattice_state,algorithem):
     '''
     For Metropolis algorithem
@@ -42,58 +27,106 @@ def spin_flip(lattice_state,algorithem):
     return new_state
 
 
-def Monte_Carlo(lattice_state_1,lattice_state_2):
-    '''
-    If the old state of lattice and a new state of lattice are given, use Metropolis Algorithm to determine whether
-    we should accept it or not.
-    arguments:
-    lattice_state_1: the state of lattice before spin change
-    lattice_state_2: the state of lattice after spin change
-    
-    return values:
-    if_accept: whether the new state(state_2) is accepted or not
-               if_accept equals to True when it is accepted and it equals to False when it is rejected.
-    current_state: if new state is accpeted, then current_state is state 2; otherwise, current_state is state 1
-    '''
-    return if_accept, current_state
+# This is an instruction to the whole project, which shows the basic structure of code and guides the following work.
+# for the functions below, we need to write arguments, return variables and a brief description for each of them.
+# unfinished yet
+
+def lattice_gerator(n): # the lattice will be represnted by a n x n matrix 
+''' 
+The initialization of lattice should be the first step of simulation. In this function, 
+we give the lattice a random initial state by built-in function: np.random.random(), the
+
+'''
+    return np.random.choice([1, -1], size=(n, n))
 
 
-
-def Ising_simulation(N,N_step,ifcorr,corr):
-    '''
-    arguments:
-    N: the number of lattice points
-    N_step: the number of simulation
-    ifcorr: whether calculate the correlation function or not
-    corr: the distance of two lattice points ??????????
-    
-    return value:
-    final_lattice_state: 
-    energy: 
-    magenetization: 
-    specific_heat: 
-    correlation_function: 
-    
-    '''
+def spin_flip(): # I dont think we need to do this in a seperate function 
+'''
+Flip a spin, namely change the state of one lattice point from +1 to -1 or from -1 to +1, 
+once a time to generate a new state of lattice.
+'''
     return
 
-def calculate_energy(lattice_state):
-    '''
-    '''
-    return energy
 
-def calculate_magnetization(lattice_state):
-    '''
-    '''
-    return magnetization
+def Monte_Carlo(): # This can also be done in the Ising_simulation function 
+'''
+If the old state of lattice and a new state of lattice are given, use Metropolis Algorithm to determine whether
+we should accept it or not.
+'''
+    return
 
-#???????maybe this part should be put inside the simulation function?????????
-def calculate_specific_heat(all_accepted_energy):
-    '''
-    '''
-    return specific_heat
+def Energy(sigma_k,sum_sigma_i,J):  # Double check if this is the correct equation to find the energy for each site.               
+'''
+This function will calculate the energy for each random spin site.
+The energy equation is written down, where sum_sigma_i represent the sum of all the neighboring spins of spin k as stated on the readMe.
+'''
+    return -J*sigma_k*sum_sigma_i
 
-def calculate_correlation_function(all_accepted_lattice_state):
+def Magnetization(lat):
+'''
+This fucntion calculate the average magnetization. lat = lattice created in the ising fucntion. 
+'''
+    return np.sum(lat)/(len(lat))
+
+
+def Ising_simulation(n, steps, J, T, r):
+    '''
+    '''
+    
+    k_b = 1 # Set the actuall bolztman constant if needed
+    
+    lattice = lattice_gerator(n)
+    energies = []
+    E0 = 0  # initial total energy
+    for i in range(n):
+        for j in range(n):
+            s_k = lattice[i][j]
+            s_i_sum = lattice[i+1][j] + lattice[i][j+1]
+            E0 += Energy(s_k,s_i_sum,J)
+    energies.append(E0)
+        
+    corr_sigma_i = []
+    corr_sigma_j = []
+    
+    
+    for step in range(steps):
+        # We will use this random generator to obtain the random indexes for the random spin site on the lattice
+        i = np.random.randint(n)
+        j = np.random.randint(n)
+        
+        s_k = lattice[i][j] # This is our random chosen spin site 
+        s_i_sum = lattice[i+1][j] + lattice[i-1][j] + lattice[i][j+1] + lattice[i][j-1] # This is the sum of the neighborin spins for the specific site
+        
+        E = Energy(s_k,s_i_sum,J)
+        
+        delta_E = E-(-E) # The energy is given by the defference between the energy of the spin original configuration 
+                         # and the energy if the spin was flip i.e changed in sign. 
+        
+        if delta_E < 0 or np.random.random() < np.exp(-delta_E/(k_b*T)): # If any of this two conditions is met, then the spin is flipped.
+            lattice[i][j] = -lattice[i][j]
+            energies.append(energies[-1]+delta_E) # This line will add the energy values for each spin site to a list which will then use to find the avarge energy
+            
+            corr_sigma_i.append(lattice[0][0])   # periodical structure ensure a random selection of one lattice point
+            coor_sigma_j.append(lattice[-r][0]) # I am not sure if this is correct
+
+        
+    # Advcice if we should use separete function to do the calculation of the evarage_energy and the evarage_energy^2.
+    Z = np.sum(np.exp(-energies/(k_b*T)))     
+    average_energy = np.sum(np.exp(-energies/(k_b*T))*energies)/Z
+    
+    average_energy_2 = np.sum(np.exp(-(energies)/(k_b*T))*(energies**2)/Z
+    
+    specific_heat = (average_energy_2 - average_energy**2)/(T**2)
+                                                                                 
+    M = Magnetization(lattice)    
+     
+    #We need to add the correlation calculations                                                                             
+    G1 = np.sum(np.exp(-energies/k_b*T)*corr_sigma_i*corr_sigma_j)/Z
+    G2 = (np.sum(np.exp(-energies/k_b*T)*corr_sigma_i)/Z)**2
+    G = G1 - G2    
+                              
+    return specific_heat, M, G 
+
 
 #Visualization part
 def plot_lattice(lattice_state):
